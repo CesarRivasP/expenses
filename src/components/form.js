16ae22f46126bcc0ac2dcfc69d52a5836b39e5f2
CesarRivasP <cesarjose58@gmail.com>
2019-06-17 07:50:22 -0400

    pass object expense for principal component

--- src/components/form.js
+++ src/components/form.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
+import shortid from 'shortid'; //shortid genera id, pero mas cortos que los de uuid
 import Error from './error';
 
 function Form(props){
 
-  const [nameExpense, saveExpense] = useState('');
+  const [nameExpense, saveNameExpense] = useState('');
   const [quantityExpense, saveQuantityExpense] = useState(0);
   const [error, saveError] = useState(false);
 
@@ -16,7 +17,20 @@ function Form(props){
       return;
     }
 
+    // construir objeto de gasto
+    const expense = {
+      id: shortid.generate(),
+      nameExpense,
+      quantityExpense
+    };
+
     // pasar el gasto al componente principal
+    props.saveExpense(expense);
+    saveError(false);
+
+    // Resetear el formulario
+    saveNameExpense('');
+    saveQuantityExpense('');
   }
 
   return(
@@ -32,7 +46,8 @@ function Form(props){
           type="text"
           className="u-full-width"
           placeholder="Ej. Transporte"
-          onChange={(e) => saveExpense(e.target.value)}
+          onChange={(e) => saveNameExpense(e.target.value)}
+          value={nameExpense}
         />
 
         <label htmlFor="quantity">Cantidad de gasto</label>
@@ -42,6 +57,7 @@ function Form(props){
           className="u-full-width"
           placeholder="Ej. 3000"
           onChange={(e) => saveQuantityExpense(Number(e.target.value))}
+          value={quantityExpense}
         />
 
         <input type="submit" value="Agregar gasto" className="u-full-width button-submit" />
