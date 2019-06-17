048f2be4ff74afa439985900143b68bbcd4fc2f0
CesarRivasP <cesarjose58@gmail.com>
2019-06-16 20:55:42 -0400

    Added state and pass setState with props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Question from './components/question';
 
 function App() {
+
+  const [budget, saveBudget] = useState(0);
+
   return (
     <div className="App container">
       <header>
         <h1>Gasto Semanal</h1>
       </header>
       <div className="contenido-principal contenido">
-        <Question />
+        <Question saveBudget={saveBudget} />
       </div>
     </div>
   );
