230a17ed28fda871b29923053549c6d617092852
CesarRivasP <cesarjose58@gmail.com>
2019-06-17 19:56:53 -0400

    Implemented deleteExpenses function

--- src/components/list.js
+++ src/components/list.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Expense from './expense';
 
-function List({expenses}){
+function List({expenses, deleteExpenses}){
   return(
     <div className="gastos-realizados">
       <h2>Listado</h2>
       {
-        expenses.map((expense) => (
-          <Expense key={expense.id} expense={expense} />
+        expenses.map((expense, index) => (
+          <Expense key={expense.id} expense={expense} index={index} deleteExpenses={deleteExpenses} />
         ))
       }
     </div>
