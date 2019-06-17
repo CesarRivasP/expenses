7f2483147efcd95a8d28453048218f19f9530f6b
CesarRivasP <cesarjose58@gmail.com>
2019-06-17 19:56:08 -0400

    Implemented new input for delete expense

--- src/components/expense.js
+++ src/components/expense.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
-const Expense = ({expense}) => (
-  <li className="gastos">
-    <p>
-      { expense.nameExpense }
-      <span className="gasto">$ {expense.quantityExpense}</span>
-    </p>
-  </li>
-);
+const Expense = ({expense, index, deleteExpenses}) => {
+  return (
+    <li className="gastos">
+      <p>
+        <span className="expense-title"><strong>{ expense.nameExpense }</strong></span>
+        <span className="gasto">$ {expense.quantityExpense}</span>
+
+        <input type="button" value="X" className="button-style" onClick={() => deleteExpenses(index, expense.quantityExpense)} />
+      </p>
+    </li>
+  );
+}
+
 
 
 export default Expense;
