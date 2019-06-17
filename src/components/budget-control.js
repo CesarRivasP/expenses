8f57499c60fd9917ca008dab319d212b0476eb55
CesarRivasP <cesarjose58@gmail.com>
2019-06-17 19:55:18 -0400

    Implemented new styles

--- src/components/budget-control.js
+++ src/components/budget-control.js
@@ -1,12 +1,13 @@
 import React, { Fragment } from 'react';
+import { reviewBudget } from '../helpers/helpers';
 
 const ControlBudget = ({budget, remaining}) => (
   <Fragment>
     <div className="alert alert-primary">
-      Presupuesto: $ {budget}
+      <strong>Presupuesto: $ { budget }</strong>
     </div>
-    <div>
-      Restante: { remaining }
+    <div className={reviewBudget(budget, remaining)}>
+      <strong>Restante: $ { remaining }</strong>
     </div>
   </Fragment>
 );
