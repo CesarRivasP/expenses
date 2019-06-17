5b62f8dac59d7291554ac6f0332fc0b22ee8532f
CesarRivasP <cesarjose58@gmail.com>
2019-06-16 20:44:23 -0400

    IMplemented state and onSubmit function and error message

--- src/components/question.js
+++ src/components/question.js
@@ -1,15 +1,32 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 function Question() {
+
+  // definir el state
+  const [ quantity, saveQuantity ] = useState(0);
+  const [ error, saveError ] = useState(false);
+
+  const addedBudget = (e) => {
+    e.preventDefault();
+
+    if(quantity <= 0 || isNaN(quantity)){ //si no es un numero retornara como true
+      saveError(true);
+      return;
+    }
+  }
+
   return (
     <Fragment>
       <h2>Ingresa tu presupuesto</h2>
-      <form>
+
+      { error && <p className="alert alert-danger error">El presupuesto es incorrecto</p> }
+
+      <form onSubmit={addedBudget}>
         <input
           type="number"
           className="u-full-width"
           placeholder="Agrega tu presupuesto"
-          // onChange={}
+          onChange={(e) => saveQuantity(Number(e.target.value))}
         />
         <input
           type="submit"
