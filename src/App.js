f57c4dfb8f332ae2f6f30115e42803dc7e0f905e
CesarRivasP <cesarjose58@gmail.com>
2019-06-16 21:03:20 -0400

    IMplemented component conditional display

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import Question from './components/question';
+// import Form from './components/form';
 
 function App() {
 
   const [budget, saveBudget] = useState(0);
+
+  const [ questionBudget, saveQuestionBudget] = useState(true)
 
   return (
     <div className="App container">
@@ -11,7 +14,19 @@ function App() {
         <h1>Gasto Semanal</h1>
       </header>
       <div className="contenido-principal contenido">
-        <Question saveBudget={saveBudget} />
+        {
+          questionBudget ?
+            <Question saveBudget={saveBudget} saveQuestionBudget={saveQuestionBudget} /> /* Para que despliegue al inicio */
+            :
+            <div className="row">
+              <div className="one-half column">
+                <p>Formulario</p>
+              </div>
+              <div className="one-half column">
+
+              </div>
+            </div>
+        }
       </div>
     </div>
   );
